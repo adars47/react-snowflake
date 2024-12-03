261645591ce3b16a5c3a4215700a2328c25facdc
adarsha Nepal <adars.nepal@gmail.com>
2024-12-03 19:00:53 +0000

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+module.exports ={
+    entry: "./src/Snowflake.js",
+    mode: 'production',
+    output: {
+        path: path.resolve("dist"),
+        filename: "main.js",
+        libraryTarget: "commonjs"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options:{
+                    presets: ["@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.css?$/,
+                use: ["style-loader","css-loader"]
+            }
+        ]
+    }
+}
